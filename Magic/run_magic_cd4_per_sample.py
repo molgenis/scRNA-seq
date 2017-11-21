b121da27ede971faefae08659cc18b6008c62c8c
Harm Brugge <harmbrugge@gmail.com>
2017-11-21 14:36:35 +0100

    Removed eqtl matrix lines

--- Magic/run_magic_cd4_per_sample.py
+++ Magic/run_magic_cd4_per_sample.py
@@ -14,10 +14,5 @@ for subdir, dirs, files in os.walk(rootdir):
         scdata.run_magic(n_pca_components=20, random_pca=True, t=4, k=9, 
                  ka=3, epsilon=1, rescale_percent=99)
         
-        # Take mean and add to eqtl_samples dataframe
-        mean = scdata.magic.data.mean()
-        mean.name = dir
-        
         # write imputed expression file per sample
         scdata.magic.data.transpose().to_csv(os.path.join(outdir, dir + ".tsv"), header=True, index=True, sep='\t', mode='w')
-
